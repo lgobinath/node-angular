b86780bb27de85cf80225043a79faa050b8a37f4
Gobinath <lgobinat@uwo.ca>
2017-11-22 22:19:27 +0000

    Add proxy to fix the error in connection

--- client/src/app/sample.service.ts
+++ client/src/app/sample.service.ts
@@ -10,7 +10,7 @@ export class SampleService {
   * This function receives a callback funtion to send back the aynchronous response from the server.
   */
   getData(callback_fun) {
-      this.http.get('https://node-angular-lgobinath.c9users.io:8081/api').subscribe(data => {
+      this.http.get('/api').subscribe(data => {
           console.log(data);
           callback_fun(data['message']);
       });
