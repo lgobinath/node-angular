42abf74ee547e5b8e42c5b671ce854f6b6db50f5
Gobinath <lgobinat@uwo.ca>
2017-11-22 22:18:34 +0000

    Make HTTP GET request from sample service

--- client/src/app/sample.service.ts
+++ client/src/app/sample.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class SampleService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
   
   /*
   * This function receives a callback funtion to send back the aynchronous response from the server.
   */
   getData(callback_fun) {
-      callback_fun('Hello World');
+      this.http.get('https://node-angular-lgobinath.c9users.io:8081/api').subscribe(data => {
+          console.log(data);
+          callback_fun(data['message']);
+      });
   }
 
 }
