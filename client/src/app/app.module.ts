0256e38bffeb831af640ca9fd422354f43df07d8
Gobinath <lgobinat@uwo.ca>
2017-11-22 21:51:44 +0000

    Add HttpClientModule dependency

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SampleComponent } from './sample/sample.component';
@@ -13,7 +13,8 @@ import {SampleService} from './sample.service'
     SampleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [SampleService],
   bootstrap: [AppComponent]
